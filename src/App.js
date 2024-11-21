270208e3b7b7610f675cf8b925d4822780c29861
Daniel Okeh <127827454+arcanecode-dev@users.noreply.github.com>
2024-11-21 17:33:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:5000')
+    fetch('https://project-1-backend-as45.onrender.com')
       .then((res) => res.text())
       .then((data) => setMessage(data));
   }, []);
